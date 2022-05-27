f74034b60b81a19c1726fadc90cbcae5a7de45d3
chunmeng fan <1416906555@qq.com>
2022-05-27 13:34:14 +0800

    1

---
+++ src/components/SearchEngine.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>
+            搜索{{msg}} <button>点击</button>
+        </h2>
+    </div>
+</template>
+
+<script>
+    // 组件交互、数据处理等功能
+    export default{
+        data(){
+           return {
+            msg:"SearchEngine"
+           }
+        },
+        method
+    }
+</script>
+
+<style>
+    /* 组件样式 */
+    .demo{
+        background-color: orange;
+    }
+</style>
