fd95120ec9ef432b53a7b2c9b3aafced3829ecc9
chunmeng fan <1416906555@qq.com>
2022-05-27 15:41:19 +0800

    搜索主页面

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <SearchEngine></SearchEngine>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import SearchEngine from './components/SearchEngineOld.vue'
+import SearchEngine from './components/SearchEngine.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    SearchEngine
   }
 }
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  background: url('~@/assets/background.png') no-repeat;
+  background-size: 100% 100%
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/SearchEngine.vue
+++ src/components/SearchEngine.vue
@@ -1,21 +1,47 @@
 <template>
     <!-- 组件的结构 -->
-    <div class="demo">
-        <h2>
-            搜索{{msg}} <button>点击</button>
-        </h2>
+    <div class="">
+        <h2>{{Msg}}</h2>
+    </div>
+    <div class="engine">
+        <p >
+            <input type="text" v-model="Query">
+            <button id="button" v-on:click="search">搜索一下</button>
+        </p>
+        
+        <p>
+            <input type="checkbox" v-model = 'NeedBlock'>需要结果过滤
+            <input type="text" v-model="Block">
+        </p>
+    </div>
+    <div class="serachres">
+        <a href="http://">这是一个搜索连接</a>
+
+        <!-- <span></span> -->
     </div>
 </template>
 
 <script>
     // 组件交互、数据处理等功能
     export default{
+        name:'SearchEngine',
         data(){
-           return {
-            msg:"SearchEngine"
-           }
+            return{
+                Msg:"Welcome to go-search!",
+                Query:'',
+                NeedBlock:'',
+                Block:'',
+            }
         },
-        method
+        methods:{
+            search(){
+                alert("Search test")
+                console.log(this.Query)
+                console.log(this.Block)
+                window.open('https://localhost:8080/query?query='+this.Query+'&block='+this.Block)
+            }
+            
+        }
     }
 </script>
 
@@ -23,5 +49,26 @@
     /* 组件样式 */
     .demo{
         background-color: orange;
+    }
+    .engine{
+        text-align: center;
+        width: auto;
+        position: 100%;
+        background-color: transparent;
+        font-size:24px;
+    }
+    .serachres{
+
+    }
+    #button{
+    /* float: right; */
+    height:'';
+    width:10%;
+    background-color:#39F;
+    border-width: 1px;
+    border-style: solid;
+    color:#FFF;
+    margin:-2px 0 0 -5px;
+    padding: 0px;
     }
 </style>
